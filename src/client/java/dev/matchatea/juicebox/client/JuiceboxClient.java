6f981e936a0dd7c973b0e3b0f7ccee7634e97765
Issac Liu <issac@matchatea.dev>
2025-08-02 02:56:00 -0400

    init: fabric

---
+++ src/client/java/dev/matchatea/juicebox/client/JuiceboxClient.java
@@ -0,0 +1,10 @@
+package dev.matchatea.juicebox.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JuiceboxClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/matchatea/juicebox/Juicebox.java
@@ -0,0 +1,10 @@
+package dev.matchatea.juicebox;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Juicebox implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
